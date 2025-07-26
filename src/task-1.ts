6110fb12475d47aa68538b1db9e9df085879f4db
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-07-26 10:50:02 +0300

    first com

---
+++ src/task-1.ts
@@ -0,0 +1,18 @@
+type Name = string;
+type Age = number;
+type IsOnline = boolean;
+
+const name: Name = "Alice";
+const age: Age = 30;
+const isOnline: IsOnline = true;
+
+/*const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+Можна записати код таким чином?
+*/
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
