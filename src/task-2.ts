572ba7fe973c367e7f6373235ccc6bfc9f07c02d
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-07-27 00:57:33 +0300

    upd t-2

--- src/task-2.ts
+++ src/task-2.ts
@@ -4,10 +4,10 @@ interface Product {
   description?: string;
 }
 
-const Product = {
+const product: Product = {
   id: 1,
   title: "Tablet",
   description: "Compact and fast",
 };
 
-console.log(`Product: ${JSON.stringify(Product)}`);
+console.log(`Product: ${JSON.stringify(product)}`);
